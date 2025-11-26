f24da7a4d85e751205b30453fabd686d6fd2b917
tae-co <dcnfsjfzp2@privaterelay.appleid.com>
2025-11-26 15:14:23 +0900

    feat:리뷰쓰기버튼기능구현

--- shoppingmall-back/src/main/java/com/shoppingmallcoco/project/repository/order/OrderRepository.java
+++ shoppingmall-back/src/main/java/com/shoppingmallcoco/project/repository/order/OrderRepository.java
@@ -3,7 +3,11 @@ package com.shoppingmallcoco.project.repository.order;
 import com.shoppingmallcoco.project.entity.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface OrderRepository extends JpaRepository<Order, Long>{
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
     List<Order> findAllByMemberMemNoOrderByOrderNoDesc(Long memNo);
+
 }
