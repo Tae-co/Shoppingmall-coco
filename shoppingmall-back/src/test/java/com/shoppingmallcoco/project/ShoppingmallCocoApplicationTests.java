accb56b80c1bd6c637680af29b4a62585a9072af
tae-co <dcnfsjfzp2@privaterelay.appleid.com>
2025-12-09 13:07:39 +0900

    refactor:test파일

--- shoppingmall-back/src/test/java/com/shoppingmallcoco/project/ShoppingmallCocoApplicationTests.java
+++ shoppingmall-back/src/test/java/com/shoppingmallcoco/project/ShoppingmallCocoApplicationTests.java
@@ -2,8 +2,12 @@ package com.shoppingmallcoco.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "jwt.secret=test-secret-key-for-ci-only"
+})
 class ShoppingmallCocoApplicationTests {
 
 	@Test
